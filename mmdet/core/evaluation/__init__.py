8bc9e74926194e506b5983fc0c47e7c1b14f1f99
chensonglu <chenslvs@163.com>
2022-04-08 16:45:24 +0800

    ccpd class

--- mmdet/core/evaluation/__init__.py
+++ mmdet/core/evaluation/__init__.py
@@ -2,7 +2,7 @@
 from .class_names import (cityscapes_classes, coco_classes, dataset_aliases,
                           get_classes, imagenet_det_classes,
                           imagenet_vid_classes, oid_challenge_classes,
-                          oid_v6_classes, voc_classes)
+                          oid_v6_classes, voc_classes, ccpd_classes)
 from .eval_hooks import DistEvalHook, EvalHook
 from .mean_ap import average_precision, eval_map, print_map_summary
 from .panoptic_utils import INSTANCE_OFFSET
